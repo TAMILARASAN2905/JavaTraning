c78559b1ae22d26df344b0a2d794678dcefbc203
tamil <tamilarsansengodan@gmail.com>
2025-09-14 01:00:18 +0530

    initial changes for java

---
+++ Demo/Basic.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class Basic {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+            System.out.println(mainMethod().toUpperCase());
+        }
+
+        public static String mainMethod () {
+            return "Kumar";
+        }
+}
